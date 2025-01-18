8c6157c4cbba3361c5f9bfef9da07d240bc91f15
Eliyaan <nopana1007@gmail.com>
2025-01-18 23:23:08 +0100

    parser works for notOnlyNots, recreates the same exact string as the input file
    next: reduce the tree

--- main.v
+++ main.v
@@ -14,6 +14,7 @@ fn string_reproduces(file string, pattern string, command string) {
 		println('reproduces')
 	} else {
 		println('does not reproduce')
+//		println(res.output)
 	}
 }
 
@@ -31,7 +32,7 @@ type Elem = string | Scope
 fn parse(file string) Scope { // The parser is surely incomplete for the V syntax, but should work for most of the cases, if not, please open an issue
 	mut stack := []&Scope{} // add the last parent to the stack
 	stack << &Scope{
-		name_id: 'BODY'
+		name_id: '' // parent
 	}
 	mut top := stack[0]
 	mut scope_level := 0 // if we are in a scope (like a function)
@@ -88,6 +89,7 @@ fn parse(file string) Scope { // The parser is surely incomplete for the V synta
 			current_string += file[i].ascii_str()
 			i++
 			scope_level += 1
+			/*
 			println("\n####new scope: ${scope_level}")
 			if stack[0].children.len > 0 {
 				println(stack[0].children.last())
@@ -95,10 +97,12 @@ fn parse(file string) Scope { // The parser is surely incomplete for the V synta
 			println('\n\n')
 			println(current_string)
 			println('\n\n')
+			*/
 		} else if file[i] == `}` {
 			current_string += file[i].ascii_str()
 			i++
 			scope_level -= 1
+			/*
 			println("\n####get out of the scope: ${scope_level}")
 			if stack[0].children.len > 0 {
 				println(stack[0].children.last())
@@ -106,6 +110,7 @@ fn parse(file string) Scope { // The parser is surely incomplete for the V synta
 			println('\n\nCurrent string:')
 			println(current_string)
 			println('\n\n')
+			*/
 			assert scope_level >= 0, 'The scopes are not well detected ${stack[0]}'
 			if scope_level == 0 && stack.len > 1 { // for the moment there are only fns
 				top.children << current_string
@@ -129,9 +134,28 @@ fn parse(file string) Scope { // The parser is surely incomplete for the V synta
 		}
 		// nothing here to avoid complexity, no need to predict what happened before, everything will be handled properly
 	}
+	top = stack[stack.len - 1]
+	top.children << current_string // last part of the file
 	assert scope_level == 0, 'The scopes are not well detected'
 	assert stack.len == 1, 'The stack should only have the BODY scope'
 	return *stack[0]
+}
+
+fn create_code(sc Scope) string {
+	mut output_code := ""
+	mut stack := []Elem{}
+	stack << sc
+	for stack.len > 0 {
+		item := stack.pop() 
+		if item is Scope {
+			stack << item.children.reverse() // to traverse the tree in the good order
+		} else if item is string { // string
+			output_code += item
+		} else {
+			panic("Should never happen")
+		}
+	}
+	return output_code
 }
 
 fn main() {
@@ -142,7 +166,9 @@ fn main() {
 
 	string_reproduces(file, 'C error found', 'v -no-skip-unused ')
 	tree := parse(file)
-	println(tree)
-
+	code := create_code(tree)
+	string_reproduces(code, 'C error found', 'v -no-skip-unused ')
+	assert code == file
+	
 	os.rmdir_all(folder) or { panic(err) }
 }
