e0f8fefe3aad3630664261e6026a689b5070650b
Eliyaan <nopana1007@gmail.com>
2025-01-18 11:14:23 +0100

    even better

--- main.v
+++ main.v
@@ -51,7 +51,7 @@ fn parse(file string) Scope { // The parser is surely incomplete for the V synta
 			i++
 			current_string += file[i].ascii_str() // maybe *
 			i++
-			for file[i - 1] != `*` && file[i] != `/` { // multiline comment -> skip next multiline end sequence
+			for !(file[i - 1] == `*` && file[i] == `/`) { // multiline comment -> skip next multiline end sequence
 				current_string += file[i].ascii_str()
 				i++
 			}
@@ -88,10 +88,24 @@ fn parse(file string) Scope { // The parser is surely incomplete for the V synta
 			current_string += file[i].ascii_str()
 			i++
 			scope_level += 1
+			println("\n####new scope: ${scope_level}")
+			if stack[0].children.len > 0 {
+				println(stack[0].children.last())
+			}
+			println('\n\n')
+			println(current_string)
+			println('\n\n')
 		} else if file[i] == `}` {
 			current_string += file[i].ascii_str()
 			i++
 			scope_level -= 1
+			println("\n####get out of the scope: ${scope_level}")
+			if stack[0].children.len > 0 {
+				println(stack[0].children.last())
+			}
+			println('\n\nCurrent string:')
+			println(current_string)
+			println('\n\n')
 			assert scope_level >= 0, 'The scopes are not well detected ${stack[0]}'
 			if scope_level == 0 && stack.len > 1 { // for the moment there are only fns
 				top.children << current_string
@@ -121,8 +135,8 @@ fn parse(file string) Scope { // The parser is surely incomplete for the V synta
 }
 
 fn main() {
-	//	file := os.read_file("../notOnlyNots/main.v")!
-	file := os.read_file('main.v')!
+	file := os.read_file("../notOnlyNots/main.v")!
+//	file := os.read_file('main.v')!
 	// parse the file to extract the scopes
 	// reduce the code first fns, then first, second... level scopes then code blocks & lines
 
