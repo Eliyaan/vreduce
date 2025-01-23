80564270dee760c752f88b12663b142d6685e5bc
Eliyaan <nopana1007@gmail.com>
2025-01-23 23:17:44 +0100

    cleanup

--- main.v
+++ main.v
@@ -1,18 +1,45 @@
 import os
 import math
 
-const folder = '.tmp_v_reduce'
-const reduced_code_file_name = '__v_reduced_code.v'
-const path = '${folder}/${reduced_code_file_name}'
-const command = 'v -no-skip-unused -w '
-const error_msg = 'C error found'
+const default_command = 'v -no-skip-unused' // Command used to compile the program, using -no-skip-unused to ease the reducing
+const default_error_msg = 'C error found' // the pattern to reproduce
+// Temporary files
+const tmp_folder = '.tmp_v_reduce'
+const tmp_reduced_code_file_name = '__v_reduced_code.v'
+const path = '${tmp_folder}/${tmp_reduced_code_file_name}'
 
+fn main() {
+	file_path := os.input('Enter the path to the file to reduce:')
+	input_error_msg := os.input('Enter the message to reproduce (empty for: ${default_error_msg}):')
+	error_msg := if input_error_msg == '' { default_error_msg } else { input_error_msg }
+	input_command := os.input('Enter the command used for reproduction (empty for: ${default_command}):')
+	command := if input_command == '' { default_command } else { input_command }
+	
+	// Opening the file
+	file := os.read_file(file_path)!
+	assert string_reproduces(file, error_msg, command)
+	println("Original code size: ${file.len}")
+	mut tree := parse(file)
+	
+	// start tests 
+	tmp_code := create_code(tree)
+	assert string_reproduces(tmp_code, error_msg, command)
+	println("Code size without comments: ${tmp_code.len}")
+
+	// reduce the code
+	reduce_scope(mut tree, error_msg, command)
+
+	// clean up
+	os.rmdir_all(tmp_folder) or { panic(err) }
+}
+
+// Return true if the command ran on the file produces the pattern
 fn string_reproduces(file string, pattern string, command string) bool {
-	if !os.exists(folder) {
-		os.mkdir(folder) or { panic(err) }
+	if !os.exists(tmp_folder) {
+		os.mkdir(tmp_folder) or { panic(err) }
 	}
 	os.write_file(path, file) or { panic(err) }
-	res := os.execute(command + path)
+	res := os.execute(command + ' ' + path)
 	if res.output.contains(pattern) {
 		//println('reproduces')
 		return true
@@ -23,44 +50,37 @@ fn string_reproduces(file string, pattern string, command string) bool {
 	}
 }
 
+type Elem = string | Scope
+
 @[heap]
 struct Scope {
 mut:
-	ignored bool // is ignored when creating the file
-	tmp_ignored bool // testing if it can be ignored in the file
-	children []Elem // code blocks & children scope
+	ignored bool // is the scope ignored when creating the file
+	tmp_ignored bool // used when testing if it can be ignored in the file
+	children []Elem // code blocks (strings & children scope
 }
 
-type Elem = string | Scope
-
-// Maybe replace mutable strings w/ byte arrays much less problems ;)
-
-fn parse(file string) Scope { // The parser is surely incomplete for the V syntax, but should work for most of the cases, if not, please open an issue
+// Parse a V file and create a scope tree to represent it
+fn parse(file string) Scope { // The parser is surely incomplete for the V syntax, but should work for most of the cases, if not, please open an issue or submit a PR
 	mut stack := []&Scope{} // add the last parent to the stack
-	stack << &Scope{}	
-	mut top := stack[0]
-	mut scope_level := 0 // if we are in a scope (like a function)
+	stack << &Scope{}
+	mut top := stack[0] // stores stack[stack.len-1] (the element on the top of the stack)
+	mut scope_level := 0 // Counts the scope depth of the current position in the file
 	mut i := 0 // index of the current char in the file
 	mut current_string := ''
 	for i < file.len {
-		top = stack[stack.len - 1]
+		top = stack[stack.len - 1] // the element on the top of the stack
 		if file[i] == `/` && file[i + 1] == `/` {
 			for file[i] != `\n` { // comment -> skip until newline
-				//current_string += file[i].ascii_str()
 				i++
 			}
 		} else if file[i] == `/` && file[i + 1] == `*` {
-			//current_string += file[i].ascii_str() // /
 			i++
-			//current_string += file[i].ascii_str() // *
 			i++
-			//current_string += file[i].ascii_str() // maybe *
 			i++
 			for !(file[i - 1] == `*` && file[i] == `/`) { // multiline comment -> skip next multiline end sequence
-				//current_string += file[i].ascii_str()
 				i++
 			}
-			//current_string += file[i].ascii_str() // /
 			i++
 		} else if file[i] == `\`` && file[i - 1] != `\\` {
 			current_string += file[i].ascii_str()
@@ -89,7 +109,7 @@ fn parse(file string) Scope { // The parser is surely incomplete for the V synta
 			}
 			current_string += file[i].ascii_str() // "
 			i++
-		} else if file[i] == `{` { // update { counter
+		} else if file[i] == `{` {
 			current_string += file[i].ascii_str()
 			i++
 			top.children << current_string
@@ -97,26 +117,8 @@ fn parse(file string) Scope { // The parser is surely incomplete for the V synta
 			current_string = ''
 			top.children << &Scope{}
 			stack << &(top.children[top.children.len - 1] as Scope)
-							/*
-							println("\n####new scope: ${scope_level}")
-							if stack[0].children.len > 0 {
-								println(stack[0].children.last())
-							}
-							println('\n\n')
-							println(current_string)
-							println('\n\n')
-							*/
 		} else if file[i] == `}` {
 			scope_level -= 1
-							/*
-							println("\n####get out of the scope: ${scope_level}")
-							if stack[0].children.len > 0 {
-								println(stack[0].children.last())
-							}
-							println('\n\nCurrent string:')
-							println(current_string)
-							println('\n\n')
-							*/
 			assert scope_level >= 0, 'The scopes are not well detected ${stack[0]}'
 			top.children << current_string
 			stack.pop()
@@ -128,7 +130,7 @@ fn parse(file string) Scope { // The parser is surely incomplete for the V synta
 			current_string += file[i].ascii_str()
 			i++
 		}
-		// nothing here to avoid complexity, no need to predict what happened before, everything will be handled properly
+		// nothing here: to avoid complexity, no need to predict what happened before in the ifs, everything will be handled properly by the ifs
 	}
 	top = stack[stack.len - 1]
 	top.children << current_string // last part of the file
@@ -137,6 +139,7 @@ fn parse(file string) Scope { // The parser is surely incomplete for the V synta
 	return *stack[0]
 }
 
+// Create the file from a scope tree
 fn create_code(sc Scope) string {
 	mut output_code := ""
 	mut stack := []Elem{}
@@ -157,27 +160,29 @@ fn create_code(sc Scope) string {
 	return output_code
 }
 
-fn reduce_scope(mut sc Scope) {
+// Reduces the code contained in the scope tree and writes the reduced code to `rpdc.v`
+fn reduce_scope(mut sc Scope, error_msg string, command string) {
+	println("Cleaning the scopes")
 	mut modified_smth := true // was a modification successful in reducing the code in the last iteration
-	for modified_smth {
+	for modified_smth { // as long as there are successful modifications
 		modified_smth = false
 		println("NEXT ITERATION")
 		mut stack := []&Elem{}
 		for i in 0 .. sc.children.len {
 			stack << &sc.children[i]
 		}
-		for stack.len > 0 {
+		for stack.len > 0 { // traverse the tree and disable (ignore) scopes that are not needed for reproduction
 			mut item := stack.pop() 
 			if mut item is Scope {
 				if !item.ignored {
-					item.tmp_ignored = true
+					item.tmp_ignored = true // try to ignore it
 					code := create_code(sc)
 					item.tmp_ignored = false // dont need it anymore
 					if string_reproduces(code, error_msg, command) {
 						item.ignored = true
 						modified_smth = true
 						println("Code size: ${code.len}")
-					} else { // if can remove it, can remove it's children 
+					} else { // if can remove it, no need to go though it's children 
 						for i in 0 .. item.children.len {
 							stack << &item.children[i]
 						}
@@ -186,6 +191,7 @@ fn reduce_scope(mut sc Scope) {
 			} 
 		}
 	}
+
 	println("Processing remaining lines")
 	tmp_code := create_code(sc).split_into_lines() // dont forget to add back the \n
 	// Create the binary tree of the lines
@@ -213,10 +219,11 @@ fn reduce_scope(mut sc Scope) {
 			}
 		}
 	}
+
+	// Traverse the tree and prune the useless lines / line groups for the reproduction
 	mut line_tree := *line_stack[0]
 	assert string_reproduces(create_code(line_tree), error_msg, command) // should be the same
 	println("Pruning the lines/line groups")
-	// Traverse the tree and prune the useless lines / line groups for the reproduction
 	modified_smth = true
 	for modified_smth {
 		modified_smth = false
@@ -225,7 +232,7 @@ fn reduce_scope(mut sc Scope) {
 		for i in 0 .. line_tree.children.len {
 			stack << &line_tree.children[i]
 		}
-		for stack.len > 0 {
+		for stack.len > 0 { // traverse the binary tree (of the lines)
 			mut item := stack.pop() 
 			if mut item is Scope {
 				if !item.ignored {
@@ -245,29 +252,9 @@ fn reduce_scope(mut sc Scope) {
 			} 
 		}
 	}
-
-	mre := create_code(line_tree)
+	
+	mre := create_code(line_tree) // final minimal reproductible example
 	assert string_reproduces(mre, error_msg, command)
 	os.write_file('rpdc.v', mre) or {panic(err)}
-}
-
-fn main() {
-	file := os.read_file("../notOnlyNots/main.v")!
-	println("Original code size: ${file.len}")
-	assert !string_reproduces(os.read_file('main.v')!, error_msg, command)
-	// parse the file to extract the scopes
-	// reduce the code first fns, then first, second... level scopes then code blocks & lines
-
-	// startup tests
-	assert string_reproduces(file, error_msg, command)
-	mut tree := parse(file)
-	code := create_code(tree)
-	assert string_reproduces(code, error_msg, command)
-	println("Code size without comments: ${code.len}")
-
-	// reduce
-	reduce_scope(mut tree)
-
-	// clean up
-	os.rmdir_all(folder) or { panic(err) }
+	println('The reduced code is now in rpdc.v')
 }
